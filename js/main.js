29c11c269fcec5ad12e102b3e95ddeb7211368fc
Mohammad Javad Ardestani <mjavad.ardestani00@gmail.com>
2021-12-01 13:09:21 +0330

    adds localstorage

--- js/main.js
+++ js/main.js
@@ -6,13 +6,34 @@ const savedAnswer = document.getElementById("saved-answer")
 let currentName = "";
 
 function submitCustom(event) {
+  console.log("submitCustom called")
   event.preventDefault()
   currentName = nameInput.value
-  const selectedGender = document.querySelector('input[name="gender"]:checked').value;
-  console.log("submitCustom called")
+  if (!currentName) {
+    return
+  }
+
+  const selectedGender = document.querySelector('input[name="gender"]:checked');
+  if(selectedGender) {
+    console.log("storing value in localstorage", currentName, selectedGender.value)
+    localStorage.setItem(currentName, selectedGender.value)
+  }
+
+  const savedGender = localStorage.getItem(currentName)
+  if (savedGender) {
+    savedAnswer.innerText = savedGender
+  } else {
+    savedAnswer.innerText = ""
+  }
 }
 
 function clearCustom(event) {
-  event.preventDefault()
   console.log("clearCustom called")
+  event.preventDefault()
+  if (!currentName) {
+    return
+  }
+  console.log("removing value from localstorage", currentName)
+  localStorage.removeItem(currentName)
+  savedAnswer.innerText = ""
 }
